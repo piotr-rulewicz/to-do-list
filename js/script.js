d6aaa5c248bb4b628d4bb54b3b4f62d611367a45
Piotr Rulewicz <piotr.rulewicz@hotmail.com>
2023-09-14 23:39:44 +0200

    Added  "Delete" ("Usuń") button

--- js/script.js
+++ js/script.js
@@ -15,16 +15,24 @@
 
     for (const task of tasks) {
       htmlString += `
-      <button class="doneButton">Zrobione</button>
         <li
           ${task.done ? " class=\"list__content--done\" " : " class=\"list__content\" "}>
+          <button class="list__removeButton js-removeButton">Usuń</button>
           ${task.content}
-        </li>
-        <button class="removeButton">Usuń</button>               
+        </li>                       
         `;
     };
 
     document.querySelector(".js-toDo").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-removeButton");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        tasks.splice(index, 1);
+        render();
+      });
+    });
   };
 
   const addNewTask = (newTask) => {
@@ -35,22 +43,24 @@
     render();
   };
 
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTask = document.querySelector(".js-writeTask").value.trim();
+
+    if (newTask === "") {
+      return;
+    }
+
+    addNewTask(newTask);
+  };
+
   const init = () => {
     render();
 
     const form = document.querySelector(".js-inputTasks");
 
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      const newTask = document.querySelector(".js-writeTask").value.trim();
-
-      if (newTask === "") {
-        return;
-      }
-
-      addNewTask(newTask);
-    });
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
